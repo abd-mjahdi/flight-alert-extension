b4f770a49dea82bffff1fb6e58987c60eba0f6e7
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-02-10 23:58:54 +0100

    prevented abusing the api in the case of quickly checking/unchecking the checkbox

--- popup.js
+++ popup.js
@@ -1,13 +1,30 @@
-persistState()
-const inputCheckBox = document.querySelector('#input');
-inputCheckBox.addEventListener("change", handleChange);
+let inputCheckBox;
 
+document.addEventListener("DOMContentLoaded", () => {
+    inputCheckBox = document.querySelector('#input');
+    inputCheckBox.addEventListener("change", handleChange);
+    persistState();
+});
+
+const interval = 30000
+let timeBetweenActivations;
+let start=Date.now();
 
 async function handleChange(){
     if(inputCheckBox.checked===true){
-    
+        
         chrome.storage.local.set({checked: true})
+
+
+        timeBetweenActivations = Date.now() - start
+        if(timeBetweenActivations<interval){
+            await sleep(interval-timeBetweenActivations)
+        }
+
+
+        
         chrome.runtime.sendMessage({checked:true})
+        start=Date.now()
 
     }else{
         chrome.storage.local.set({checked: false})
@@ -17,8 +34,11 @@ async function handleChange(){
 
 async function persistState(){
     let state = await chrome.storage.local.get(['checked'])
-    inputCheckBox.checked=state.checked
+    inputCheckBox.checked=state.checked || false
+    console.log(state)
 }
 
-
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve,ms))
+}
 
